7687792332150120fbba6591917a5edb2c0bc7aa
Ivan <mmavrin80@gmail.com>
2022-03-19 22:21:38 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите трёхзначное число: ");
+string n = Console.ReadLine();
+int Num = int.Parse(n);
+
+int Num1 = Num / 100;
+int Num3 = Num % 10; 
+int res = Num1 * 10 + Num3;
+
+Console.Write(res);
