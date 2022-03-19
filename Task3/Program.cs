c6b522c00ce659b453db19fd1f764e73d9720f94
Ivan <mmavrin80@gmail.com>
2022-03-19 23:15:40 +0300

    task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите число: ");
+string n = Console.ReadLine();
+int Num = int.Parse(n);
+
+
+int i = 100;
+int num = 0;
+int result = 0;
+if (Num < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while (Num / i > 9)
+    { 
+        i = i * 10;
+    }
+    i = i / 100;
+    int Num1 = Num / i;
+    result = Num1 % 10;
+    Console.WriteLine(result);
+}
+
+
