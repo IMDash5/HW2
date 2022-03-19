37636ab9f7e48e9b6e4c4b5d74622684b2bb927d
Ivan <mmavrin80@gmail.com>
2022-03-19 23:35:33 +0300

    Task4

---
+++ Task 4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите цифру дня недели: ");
+string n = Console.ReadLine();
+int Num = int.Parse(n);
+
+if (Num == 7)
+{
+    Console.Write("Это выходной");
+}
+else
+{
+    if (Num == 6)
+    {
+        Console.Write("Это выходной");
+    }
+    else 
+    {
+        Console.Write("Это не выходной");
+    }
+}
